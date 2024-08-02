039ac3721d5783ab8d636a2c4fcdc7bd7370191a
MohamedElAllouli <alloulialloulimohamed2023@gmail.com>
2024-08-01 17:10:13 -0700

    tasks

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Basic annotations - to string
+Write typed-annotation function to_str that takes a float argument
+Returns string repr of the float
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Type-annotated function
+    """
+    return str(n)
---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Annotate a function param and return values
+with appropriate types
+"""
+from typing import Sequence, Iterable, List, Tuple
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """
+    Annotate function param
+    element_length
+    """
+    return [(i, len(i)) for i in lst]
